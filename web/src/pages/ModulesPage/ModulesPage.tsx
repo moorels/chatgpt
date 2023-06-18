b63cb23fa1a68e42c79bdc68d3437e9a26fb7b6e
moorels <sales@micropls.com>
2023-06-18 15:50:42 +1000

    chat12

--- web/src/pages/ModulesPage/ModulesPage.tsx
+++ web/src/pages/ModulesPage/ModulesPage.tsx
@@ -1,13 +1,13 @@
 const ModulesPage = () => {
   return (
     <>
-      <div className=" h-screen">
+      <div className="">
         <div className="ml-3 font-libre text-xl text-slate-950">Chat Gpt</div>
         <iframe
-          src="https://chatbot3-one.vercel.app/"
+          src="https://joshchatbot.vercel.app/"
           title="Embedded Content"
-          width="1200"
-          height="1200"
+          width="800"
+          height="450"
         />
       </div>
     </>
