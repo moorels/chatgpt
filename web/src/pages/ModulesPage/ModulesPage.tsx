b788a5fc4ce7d09ddbcb0205519b1eab72df2682
moorels <sales@micropls.com>
2023-07-02 14:49:38 +1000

    com16

--- web/src/pages/ModulesPage/ModulesPage.tsx
+++ web/src/pages/ModulesPage/ModulesPage.tsx
@@ -1,62 +1,65 @@
 import { Link, routes } from '@redwoodjs/router'
 
-/* eslint-disable jsx-a11y/iframe-has-title */
 const ModulesPage = () => {
   return (
     <>
-      <div className="flex-col bg-slate-700  py-2">
-        <div className=" bg-slate-700 text-center">
-          <div className="  flex-col items-center bg-slate-700 text-center">
-            <nav className=" justify-left mb-2 flex flex-wrap items-center px-12 text-base md:ml-4">
-              <ul>
-                <li>
-                  <div className="space-x-1 ">
-                    <Link
-                      className="  focus:shadow-outline  mb-6 bg-gray-500 p-2 font-libre text-indigo-100 transition-colors duration-150 hover:bg-zinc-600"
-                      to={routes.computer()}
-                    >
-                      <span className=" relative inline-flex h-2 w-2  bg-green-500">
-                        <span className=" absolute inline-flex h-full w-full  bg-gray-100 opacity-75"></span>
-                      </span>
-                      <span> Computer Store</span>
-                    </Link>
-                    <Link
-                      className="  focus:shadow-outline mb-6 bg-gray-500 p-2 font-libre text-indigo-100 transition-colors duration-150 hover:bg-zinc-600"
-                      to={routes.store2()}
-                    >
-                      <span className=" relative inline-flex h-2 w-2  bg-green-500">
-                        <span className=" absolute inline-flex h-full w-full   bg-gray-100 opacity-75"></span>
-                      </span>
-                      <span> Dog Grooming</span>
-                    </Link>
-                    <Link
-                      className="  focus:shadow-outline mb-6 bg-gray-500 p-2 font-libre text-indigo-100 transition-colors duration-150 hover:bg-zinc-600"
-                      to={routes.store3()}
-                    >
-                      <span className=" relative inline-flex h-2 w-2  bg-green-500">
-                        <span className=" absolute inline-flex h-full w-full   bg-gray-100 opacity-75"></span>
-                      </span>
-                      <span> Florist</span>
-                    </Link>
-                  </div>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className=" bg-white  py-2">
-            <div className="flex-col bg-slate-700  py-1">
-              <iframe
-                className=" m-0 -mt-[5px] bg-slate-700 p-0"
-                src="https://chatstore1.vercel.app"
-                height="650"
-                width="620"
-              ></iframe>
+      <div className="body-font  bg-slate-600  text-gray-600">
+        <header className="body-font bg-slate-600  text-gray-600">
+          <div className="  flex flex-col   p-3 sm:flex-row lg:flex-row ">
+            <ul>
+              <div className=" bg-white-500 place-content-center p-1 text-white">
+                <Link
+                  className="  focus:shadow-outline  bg-gray-500 p-2 font-libre text-indigo-100 transition-colors duration-150 hover:bg-zinc-600"
+                  to={routes.computer()}
+                >
+                  <span className=" relative inline-flex h-2 w-2  bg-green-500">
+                    <span className=" absolute inline-flex h-full w-full  bg-gray-100 opacity-75"></span>
+                  </span>
+                  <span> Computer Store</span>
+                </Link>
+              </div>
+            </ul>
+
+            <div className=" bg-white-500 place-content-center p-1 text-white">
+              <Link
+                className="  focus:shadow-outline  bg-gray-500 p-2 font-libre text-indigo-100 transition-colors duration-150 hover:bg-zinc-600"
+                to={routes.store2()}
+              >
+                <span className=" relative inline-flex h-2 w-2  bg-green-500">
+                  <span className=" absolute inline-flex h-full w-full   bg-gray-100 opacity-75"></span>
+                </span>
+                <span> Dog Grooming</span>
+              </Link>
             </div>
+            <div className=" bg-white-500   place-content-center p-1 text-white">
+              <Link
+                className="  focus:shadow-outline  bg-gray-500 p-2 font-libre text-indigo-100 transition-colors duration-150 hover:bg-zinc-600"
+                to={routes.store3()}
+              >
+                <span className=" relative inline-flex h-2 w-2  bg-green-500">
+                  <span className=" absolute inline-flex h-full w-full   bg-gray-100 opacity-75"></span>
+                </span>
+                <span> Florist</span>
+              </Link>
+            </div>
+          </div>
+          <div className=" focus:shadow-outline m-0 w-60 justify-center rounded-lg p-0 text-center align-middle text-2xl text-slate-100"></div>
+        </header>
+      </div>
+      <div>
+        <div className="  bg-white py-2">
+          <div className="h-screen flex-col  bg-slate-700 py-1">
+            <iframe
+              className=" m-0 -mt-[5px] bg-slate-700 p-0"
+              title="df"
+              src="https://chatstore1.vercel.app"
+              height="650"
+              width="620"
+            ></iframe>
           </div>
         </div>
       </div>
     </>
   )
 }
-
 export default ModulesPage
