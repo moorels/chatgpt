21f600312b3449293d87f7b20f553da6a4545e98
moorels <sales@micropls.com>
2023-07-02 12:39:24 +1000

    com14

--- web/src/layouts/SkelLayout/SkelLayout.tsx
+++ web/src/layouts/SkelLayout/SkelLayout.tsx
@@ -36,18 +36,17 @@ const SkelLayout = ({ children }: SkelLayoutProps) => {
       <div className="body-font  bg-slate-600  text-gray-600">
         <header className="body-font bg-slate-600  text-gray-600">
           <div className="  flex flex-col flex-wrap items-center p-3 md:flex-row ">
-            <div className="title-font mb-0 flex items-center px-2 py-2 font-medium text-gray-900 md:mb-0">
+            <div className="title-font mb-0 flex items-center font-medium text-gray-900 md:mb-0 ">
               <Link to={routes.homepage()}>
                 <svg fill="none" viewBox="0 0 24 24" height="2em" width="2em">
                   <path
-                    fill="silver"
+                    fill="currentColor"
                     fillRule="evenodd"
                     d="M9 22a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2z"
                     clipRule="evenodd"
                   />
                   <path
-                    stroke="silver"
-                    fill="gray"
+                    fill="currentColor"
                     fillRule="evenodd"
                     d="M9 2a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zM9 22a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zm7-7a1 1 0 10-2 0 1 1 0 002 0zm0-4a1 1 0 10-2 0 1 1 0 002 0zm-1-5a1 1 0 110 2 1 1 0 010-2zM2 15a1 1 0 110 2 1 1 0 010-2zm0-4a1 1 0 110 2 1 1 0 010-2zm1-3a1 1 0 10-2 0 1 1 0 002 0zm14-2H7a1 1 0 00-1 1v10a1 1 0 001 1h10a1 1 0 001-1V7a1 1 0 00-1-1zM7 4a3 3 0 00-3 3v10a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H7zm7 6h-4v4h4v-4zM8 8v8h8V8H8z"
                     clipRule="evenodd"
